14c9a830c43b9f51c4bd3f8d85986f5eb14588f9
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-01 17:42:08 +0700

    Replace with actual BIP39 implementation

--- lib/App/BIP39Utils.pm
+++ lib/App/BIP39Utils.pm
@@ -3,60 +3,6 @@ package App::BIP39Utils;
 # DATE
 # VERSION
 
-use 5.010001;
-use strict;
-use warnings;
-
-our %SPEC;
-
-$SPEC{gen_bip39_mnemonic_phrase} = {
-    v => 1.1,
-    summary => 'Generate BIP39 mnemonic phrase',
-    args => {
-        language => {
-            summary => 'Pick which language wordlist to use',
-            schema => ['str*', match=>qr/\A\w{2}(?:-\w+)?\z/],
-            default => 'en',
-            description => <<'_',
-
-Will retrieve wordlist from `WordList::<LANG_CODE>::BIP39` Perl module.
-
-For Chinese (simplified), use `zh-simplified`. For Chinese (traditional), use
-`zh-traditional`.
-
-_
-        },
-        length => {
-            summary => 'Number of words to produce',
-            schema => ['posint*', max=>128],
-            default => 12,
-        },
-    },
-};
-sub gen_bip39_mnemonic_phrase {
-    require Math::Random::Secure;
-
-    my %args = @_;
-
-    my ($langcode, $variant) = ($args{language} // 'en') =~ /\A(\w{2})(?:-(\w+))?\z/
-        or return [400, "Invalid language '$args{language}', please specify a 2-digit language code"];
-    my $mod = "WordList::".uc($langcode).($variant ? "::".ucfirst(lc($variant)) : "")."::BIP39";
-    (my $mod_pm = "$mod.pm") =~ s!::!/!g;
-    require $mod_pm;
-
-    my @all_words = $mod->new->all_words;
-
-    my @words;
-    my $length = $args{length} // 12;
-    for (1..$length) {
-        my $word = $all_words[@all_words * Math::Random::Secure::rand()];
-        redo if grep { $word eq $_ } @words;
-        push @words, $word;
-    }
-
-    [200, "OK", join(" ", @words)];
-}
-
 1;
 # ABSTRACT: Collection of CLI utilities related to BIP39
 
